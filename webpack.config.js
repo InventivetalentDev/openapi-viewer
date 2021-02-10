5f560df92b2a4df428dc7999c071c5c64229cbaa
inventivetalent <mail@inventivetalent.org>
2021-02-10 14:01:31 +0100

    images

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path:  path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     module: {
@@ -24,6 +25,12 @@ module.exports = {
     plugins: [
         new webpack.ProgressPlugin(),
         new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: "./src/img", to: "img"},
+                {from: "./src/favicon.ico", to: "favicon.ico"}
+            ]
+        }),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })

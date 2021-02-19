d983970a75ddeb7dc89b43e9598d4813a27ecd09
inventivetalent <mail@inventivetalent.org>
2021-02-20 00:29:54 +0100

    move definition to search & support for auto-expanding hash

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import SwaggerUI from "swagger-ui";
 import 'swagger-ui/dist/swagger-ui.css';
 import './style.css'
 
-const definition = window.location.hash.substr(1);
+const definition = window.location.search.substr(1) || window.location.hash.substr(1);
 
 const wrapper = document.getElementById("input-wrapper");
 const input = document.getElementById("input");
@@ -37,10 +37,19 @@ function load(definition) {
     window.ui = SwaggerUI({
         dom_id: "#ui",
         url: definition,
+        docExpansion: "list",
         onComplete: function (api, ui) {
             console.log(api);
             console.log(ui);
             wrapper.style.display = "none";
+
+            let hash = window.location.hash;
+            if (!hash.startsWith("#http")) {
+                let idEl = document.querySelector(hash);
+                if (idEl) {
+                    idEl.querySelector(".opblock-summary").click();
+                }
+            }
         },
         onFailure: function () {
             wrapper.style.display = "";

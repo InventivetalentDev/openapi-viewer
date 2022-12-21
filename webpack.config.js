397968a3abeb95eec895b0f052584ba5e8821e16
inventivetalent <mail@inventivetalent.org>
2022-12-21 12:17:27 +0100

    add publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: "/"
     },
     module: {
         rules: [
